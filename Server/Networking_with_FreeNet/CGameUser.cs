e134e09e2fac4b676b9f4097ee59a1ecf4c08abb
Jeong Hyeon <rhea31@naver.com>
2019-01-20 09:59:23 +0900

    Basic Setting

---
+++ Server/Networking_with_FreeNet/CGameUser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FreeNet;
+
+namespace Networking_with_FreeNet
+{
+    class CGameUser : IPeer
+    {
+        CUserToken token;
+
+        public CGameUser(CUserToken token)
+        {
+            this.token = token;
+            this.token.set_peer(this);
+        }
+
+        void IPeer.on_removed()
+        {
+            Program.remove_user(this);
+        }
+
+        public void send(CPacket msg)
+        {
+            msg.record_size();
+            this.token.send(new ArraySegment<byte>(msg.buffer, 0, msg.position));
+        }
+
+        public void send(ArraySegment<byte> data)
+        {
+            this.token.send(data);
+        }
+
+        void IPeer.disconnect()
+        {
+            this.token.ban();
+        }
+
+        void IPeer.on_message(CPacket msg)
+        {
+            
+        }
+    }
+}
---
+++ Server/Networking_with_FreeNet/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreeNet;
+
+namespace Networking_with_FreeNet
+{
+	class Program
+	{
+		static List<CGameUser> userlist;
+
+		static void Main(string[] args)
+		{
+			userlist = new List<CGameUser>();
+
+			CNetworkService service = new CNetworkService(false);
+			service.session_created_callback += on_session_created;
+            service.initialize(10000, 1024);
+			service.listen("0.0.0.0", 7979, 100);
+            
+            service.disable_heartbeat();
+
+
+			Console.WriteLine("Started!");
+			while (true)
+			{
+                //Console.Write(".");
+                string input = Console.ReadLine();
+                if (input.Equals("users"))
+                {
+                    Console.WriteLine(service.usermanager.get_total_count());
+                }
+				System.Threading.Thread.Sleep(1000);
+			}
+		}
+		static void on_session_created(CUserToken token)
+		{
+            CGameUser user = new CGameUser(token);
+            lock (userlist)
+            {
+                userlist.Add(user);
+            }
+        }
+
+		public static void remove_user(CGameUser user)
+		{
+            lock (userlist)
+            {
+                userlist.Remove(user);
+            }
+        }
+	}
+}
